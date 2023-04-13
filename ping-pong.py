c94a4e31cdd6baf118026b9354dd4740170fac87
matvey-gorunov <129772001+matvey-gorunov@users.noreply.github.com>
2023-04-13 19:03:45 +0300

    Add files via upload
    итог

--- ping-pong.py
+++ ping-pong.py
@@ -8,12 +8,15 @@ ball_image = pygame.image.load('ping-pong-ball.png')
 ball_image = pygame.transform.scale(ball_image,(81,81))
 background_image = pygame.image.load('background.jpg')
 background_image = pygame.transform.scale(background_image,(700,500))
+pygame.init()
+text = pygame.font.SysFont('Arial',50,1)
 timer = pygame.time.Clock()
 class Object():
     def __init__(self,x,y,image,xs,ys):
         self.rect = image.get_rect(center = (x,y))
         self.xs = xs
         self.ys = ys
+        self.score = 0
 class Player(Object):
     def control_1(self):
         key_list = pygame.key.get_pressed()
@@ -39,16 +42,19 @@ class Ball(Object):
             self.xs = -self.xs
             self.rect.centerx = 350
             self.rect.centery = 250
+            player01.score += 1
             print(self.rect.x,self.xs)
         if self.rect.x == 0:
             self.xs = -self.xs
             self.rect.centerx = 350
             self.rect.centery = 250
+            player02.score += 1
     def collision(self):
         if self.rect.colliderect(player01.rect):
             self.xs = 10
         if self.rect.colliderect(player02.rect):
             self.xs = -10
+
             
 
         
@@ -62,10 +68,29 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+    text_image01 = text.render(str(player01.score),0,(100,0,255))
+    text_image02 = text.render(str(player02.score),0,(100,0,255))
+    if player01.score == 3:
+        win01 = text.render('1-ый игрок выиграл',0,(100,0,0))
+    if player02.score == 3:
+        win02 = text.render('2-ый игрок выиграл',0,(100,0,0))
+
     window.blit(background_image,(0,0))
     window.blit(racket_image,player01.rect)
     window.blit(racket_image2,player02.rect)
     window.blit(ball_image,ball01.rect)
+    window.blit(text_image01,(200,0))
+    window.blit(text_image02,(450,0))
+    if player01.score == 3:
+        win01 = text.render('1-ый игрок выиграл',0,(100,0,0))
+        window.blit(win01,(150,300))
+        ball01.xs = 0
+        ball01.ys = 0
+    if player02.score == 3:
+        win02 = text.render('2-ый игрок выиграл',0,(100,0,0))
+        window.blit(win02,(150,300))
+        ball01.xs = 0
+        ball01.ys = 0
     player01.control_1()
     player02.control_2()
     ball01.move()
